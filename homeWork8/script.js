4fad443832c886d229af61893491f42b55d831c4
kris30may1 <51117939+kris30may1@users.noreply.github.com>
2020-01-13 10:47:32 +0200

    Update script.js

--- homeWork8/script.js
+++ homeWork8/script.js
@@ -2,24 +2,32 @@
 
 const taskInput = document.querySelector('#toDoInput');
 const toDoList = document.querySelector('#toDoList');
-const arrOfLi = document.querySelectorAll('#toDoList');
 const liTemplate = document.querySelector('#liTemplate').innerHTML;
+const errorMessage = document.getElementById('errorMessage');
+const defColor = toDoList.style.backgroundColor;
 
 document.querySelector('#toDoForm').addEventListener('submit', onToDoFormSubmit);
 document.querySelector('#addBtn').addEventListener('click', onToDoFormSubmit);
-toDoList.addEventListener('click', onLiElmClick, false);
+toDoList.addEventListener('click', onLiElmClick);
 
 
 function onToDoFormSubmit(e){
     e.preventDefault();
-    createToDoList();
+    const task = getTaskStr();
+    if(validateNewTask(task)){
+        createToDoList();
+    } else {
+        showError();
+    }
     clear();
     focus();
 }
 
 function onLiElmClick(e) {
+if(e.target.style.backgroundColor == defColor)
     e.target.style.backgroundColor = 'green';
-  }
+else{e.target.style.backgroundColor = defColor;}	
+}
 
 function createToDoList(){
     recTaskinToDoList();
@@ -38,9 +46,9 @@ function addToDo(){
     return liTemplate.replace('{{newTask}}', getTaskStr());
 }
 
-// function validateNewTask(task) {
-//     return task != '';
-// }
+function validateNewTask(task) {
+    return task != '';
+}
 
 function focus(){
     taskInput.focus();
@@ -48,4 +56,12 @@ function focus(){
 
 function clear(){
     taskInput.value = '';
+}
+
+function showError(){
+    errorMessage.hidden = false;
+}
+
+function hideError(){
+    errorMessage.hidden = true;
 }
