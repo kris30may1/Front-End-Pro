d519fb99a1e03eba34d1bc0eeb9faf63fa117d86
kris30may1 <khrystyna.korzh@gmail.com>
2020-01-18 22:24:08 +0200

    hw done

---
+++ homeWork10/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+function createCalculator(num){
+    return {
+        set: value => (num = value),
+        get: () => num,
+        add: (value) => num + value,
+        sub: (value) => num - value,
+        divide: (value) => num / value,
+        mult: (value) => num * value
+    }
+}
+
+const calculator = createCalculator(10); 
+
+console.log(calculator.add(45)); // возвращает 55 
+console.log(calculator.sub(45)); // возвращает -35 
+console.log(calculator.divide(5)); // возвращает 2 
+console.log(calculator.mult(5)); // возвращает 50 
+console.log(calculator.set(100)); // устанавливает базовое значение в 100 
+console.log(calculator.mult(5)); // возвращает 500
