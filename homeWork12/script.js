caaa2e065ed7995877664e9994368117ccad1d8d
kris30may1 <khrystyna.korzh@gmail.com>
2020-01-27 21:54:17 +0200

    hw

--- homeWork12/script.js
+++ homeWork12/script.js
@@ -2,44 +2,33 @@
 
 class Hamburger {
     
-    static SIZE_SMALL = {price: 50,
-                         calories = 20};
-    static SIZE_MIDDLE = {price: 75,
-                          calories: 30};
-    static SIZE_LARGE = {price: 100,
-                         calories: 40};
+    static SIZE_SMALL = { price: 50, calories: 20 };
+    static SIZE_MIDDLE = { price: 75, calories: 30 };
+    static SIZE_LARGE = { price: 100, calories: 40 };
 
-    static ADDITION_CHEESE = {price: 10,
-                              calories: 20};
-    static ADDITION_SALAD = {price: 20,
-                            calories: 5};
-    static ADDITION_POTATO = {price: 15,
-                              calories: 5};
-    static ADDITION_SPICE = {price: 15,
-                             calories: 0};
-    static TOPING_MAYO = {price: 20,
-                          calories: 5};
+    static ADDITION_CHEESE = { price: 10, calories: 20 };
+    static ADDITION_SALAD = { price: 20, calories: 5 };
+    static ADDITION_POTATO = { price: 15, calories: 5 };
+    static ADDITION_SPICE = { price: 15, calories: 0 };
+    static TOPING_MAYO = { price: 20, calories: 5 };
     
-    constructor(hamburgerPrice, hamburgerCalories) {
-        this.hamburgerPrice = Hamburger.SIZE_SMALL.price || Hamburger.SIZE_MIDDLE.price || Hamburger.SIZE_LARGE.price;
-        this.hamburgerCalories = Hamburger.SIZE_SMALL.calories || Hamburger.SIZE_MIDDLE.calories || Hamburger.SIZE_LARGE.calories;
+    constructor(size, additions) {
+        this.size = size;
+        this.additions = [];
     }
 
     add(addition) {
-        const addPrice = addition.price;
-        const addCalories = addition.calories;
-        this.addPrice = addPrice;
-        this.addCalories = addCalories;
+        this.additions.push(addition);
     }
 
     calculatePrice() {
-        this.hamburgerPrice += this.addPrice;
-        return this.hamburgerPrice; 
+        return this.additions.reduce((sum, addition) => {
+            return sum + addition}, this.size.price);
     }
 
     calculateCalories() {
-        this.hamburgerCalories += this.addCalories;
-        return this.hamburgerCalories;
+        return this.additions.reduce((sum, addition) => {
+            return sum + addition}, this.size.calories);
     }
 }
 
