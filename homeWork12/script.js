efce471bba443b8057aa2ee7bf0c126202fb65a7
kris30may1 <khrystynar@leadsmarket.com>
2020-01-27 12:11:22 +0200

    hw done

--- homeWork12/script.js
+++ homeWork12/script.js
@@ -25,21 +25,22 @@ class Hamburger {
     }
 
     calculatePrice() {
-        const price = this.hamburgerPrice + this.addPrice;
-        return price; 
+        this.hamburgerPrice += this.addPrice;
+        return this.hamburgerPrice; 
     }
 
     calculateCalories() {
-        const calories = this.hamburgerCalories + this.addCalories;
-        return calories;
+        this.hamburgerCalories += this.addCalories;
+        return this.hamburgerCalories;
     }
 }
 
 const hamburger = new Hamburger (Hamburger.SIZE_SMALL);
 
-console.log(hamburger);
-console.log(hamburger.add(Hamburger.TOPING_MAYO));
-console.log('Calories: ' + hamburger.calculateCalories());
-console.log('Price: ' + hamburger.calculatePrice());
-console.log(hamburger.add(Hamburger.ADDITION_SPICE));
-console.log('Price with spice: ' + hamburger.calculatePrice()) 
+console.log(hamburger); // маленький гамбургер
+console.log(hamburger.add(Hamburger.TOPING_MAYO)); // добавка из майонеза 
+console.log('Calories: ' + hamburger.calculateCalories()); // калорийность гамбургера с майонезом
+console.log('Price: ' + hamburger.calculatePrice()); // цена гамбургера с майонезом
+console.log(hamburger.add(Hamburger.ADDITION_SPICE)); // добавим приправы
+console.log('Price with spice: ' + hamburger.calculatePrice()); // цена гамбургера с майонезом и приправой
+console.log('Calories with spice: ' + hamburger.calculateCalories()); // калорийность гамбургера с майонезом и приправой
