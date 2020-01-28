0a6bbc5e783f51f6b20d0946639b2736721308b8
kris30may1 <khrystyna.korzh@gmail.com>
2020-01-28 22:45:22 +0200

    hw new v

--- homeWork13/TabSet.js
+++ homeWork13/TabSet.js
@@ -3,10 +3,11 @@ class TabSet {
         this.el = el;
         this.init();
     }
-
-    static TAB_CONTAINER_CLASS = 'tab-container';
-    static TAB_ITEM_CLASS = 'tab-item';
-    static TAB_ITEM_CONTENT_CLASS = 'tab-item-content';
+    
+    static TAB_CONTAINER_CLASS = 'nav';
+    static TAB_ITEM_CLASS = 'nav-item';
+    static TAB_ITEM_LINK_CLASS = 'nav-link';
+    static TAB_ITEM_CONTENT_CLASS = 'nav-content';
 
     init() {
         this.bindClasses();
@@ -16,6 +17,8 @@ class TabSet {
         this.el.classList.add(TabSet.TAB_CONTAINER_CLASS);
         Array.prototype.forEach.call(this.el.children, itemEl => {
             itemEl.classList.add(TabSet.TAB_ITEM_CLASS);
+            itemEl.children[0].classList.add(TabSet.TAB_ITEM_LINK_CLASS);
+            itemEl.children[1].classList.add(TabSet.TAB_ITEM_CONTENT_CLASS);
         }) 
     }
 
