ed9ee55cf97a23e1007864a931eda052df567280
kris30may1 <khrystynar@leadsmarket.com>
2020-01-28 09:58:30 +0200

    hw done

--- homeWork12/script.js
+++ homeWork12/script.js
@@ -2,42 +2,42 @@
 
 class Hamburger {
     
+    constructor(size) {
+        this.size = size;
+        this.additions = [];
+    }
+
     static SIZE_SMALL = { price: 50, calories: 20 };
     static SIZE_MIDDLE = { price: 75, calories: 30 };
     static SIZE_LARGE = { price: 100, calories: 40 };
-
     static ADDITION_CHEESE = { price: 10, calories: 20 };
     static ADDITION_SALAD = { price: 20, calories: 5 };
     static ADDITION_POTATO = { price: 15, calories: 5 };
     static ADDITION_SPICE = { price: 15, calories: 0 };
     static TOPING_MAYO = { price: 20, calories: 5 };
     
-    constructor(size, additions) {
-        this.size = size;
-        this.additions = [];
+    calculatePrice() {
+        return this.additions.reduce((sum, addition) => {
+            return sum + addition.price;
+        }, this.size.price);
+    }
+
+    calculateCalories() {
+        return this.additions.reduce((sum, addition) => {
+            return sum + addition.calories;
+        }, this.size.calories);
     }
 
     add(addition) {
         this.additions.push(addition);
     }
-
-    calculatePrice() {
-        return this.additions.reduce((sum, addition) => {
-            return sum + addition}, this.size.price);
-    }
-
-    calculateCalories() {
-        return this.additions.reduce((sum, addition) => {
-            return sum + addition}, this.size.calories);
-    }
 }
 
 const hamburger = new Hamburger (Hamburger.SIZE_SMALL);
+hamburger.add(Hamburger.TOPING_MAYO); // добавка из майонеза 
 
-console.log(hamburger); // маленький гамбургер
-console.log(hamburger.add(Hamburger.TOPING_MAYO)); // добавка из майонеза 
 console.log('Calories: ' + hamburger.calculateCalories()); // калорийность гамбургера с майонезом
 console.log('Price: ' + hamburger.calculatePrice()); // цена гамбургера с майонезом
-console.log(hamburger.add(Hamburger.ADDITION_SPICE)); // добавим приправы
+hamburger.add(Hamburger.ADDITION_SPICE); // добавим приправы
 console.log('Price with spice: ' + hamburger.calculatePrice()); // цена гамбургера с майонезом и приправой
 console.log('Calories with spice: ' + hamburger.calculateCalories()); // калорийность гамбургера с майонезом и приправой
