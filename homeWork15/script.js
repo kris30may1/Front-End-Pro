af7f08479110cf2786bea60cc8d195e494d0112d
kris30may1 <khrystynar@leadsmarket.com>
2020-02-06 18:07:48 +0200

    new v

--- homeWork15/script.js
+++ homeWork15/script.js
@@ -1,27 +1,47 @@
 'use strict'
 
 const listOfAlbums = document.querySelector('#albums');
+const gallery = document.querySelector('#gallery');
 const liTemplate = document.querySelector('#liTemplate').innerHTML;
+const divTemplate = document.querySelector('#divTemplate').innerHTML;
 
-const fetchTitles = fetch('https://jsonplaceholder.typicode.com/albums');
-    console.log(fetchTitles);
-    fetchTitles.then(response => response.json())
-    .then(albums => {
-        const titles = albums.map(
-            album => generateLi(album.title)
-        ).join('\n');
-        listOfAlbums.innerHTML = titles;
-    })
+const ALBUMS_URL = 'https://jsonplaceholder.typicode.com/albums';
+const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos';
 
 listOfAlbums.addEventListener('click', onAlbumItemClick);
 
-function onAlbumItemClick(e){
-    if (e.target) {
+getAlbums();
 
-    }
+function getAlbums(){
+    fetch(ALBUMS_URL)
+        .then(response => response.json())
+        .then(albums => {
+        const titles = albums.map(
+            album => generateLi(album)
+        ).join('\n');
+        listOfAlbums.innerHTML = titles;
+    }) 
 }
 
-function generateLi(album){
-    return liTemplate.replace('{{newAlbum}}', album)
-                     .replace('{{id}}');
+function onAlbumItemClick(e) {
+    getPhotos(e.target);
+}
+
+function getPhotos(el) {
+    fetch(`${PHOTOS_URL}?albumId=${el.dataset.id}`)
+        .then(response => response.json())
+        .then(photos => {const urls = photos.map(
+            photo => generateDiv(photo)
+        ).join('\n');
+            gallery.innerHTML = urls;
+        })
+}
+
+function generateDiv(el) {
+    return divTemplate.replace('{{photoUrl}}', el.thumbnailUrl);
+}
+
+function generateLi(album) {
+    return liTemplate.replace('{{newAlbum}}', album.title)
+                     .replace('{{id}}', album.id);
 }
