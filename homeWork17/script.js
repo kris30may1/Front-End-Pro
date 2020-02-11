3d9c05eb4f31ff46e4749c36e3f2325cddb597aa
kris30may1 <khrystynar@leadsmarket.com>
2020-02-11 17:43:43 +0200

    hw version 1

---
+++ homeWork17/script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const addNewTaskBtn = document.querySelector('#add-new-task-btn');
+
+addNewTaskBtn.addEventListener('click', onAddNewTaskBtnClick);
+
+function onAddNewTaskBtnClick() {
+    setNewTask();
+}
+
+function setNewTask() {
+    
+}
