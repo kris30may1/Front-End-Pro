94c8c797a5493bdc552c312a725798f1374bce74
kris30may1 <khrystyna.korzh@gmail.com>
2020-03-12 20:40:36 +0200

    hw

--- homeWork23/src/gallery-data.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-// let photos = [];
-
-module.exports = data => (photos = data);
--- homeWork23/src/gallery.js
+++ homeWork23/src/gallery.js
@@ -1,5 +1,17 @@
 'use strict'
 
+require(['./lightgallery.js'], function() {
+    require(["./lg-zoom.js", "./lg-thumbnail.js"], function(){
+        $("#lightgallery").lightGallery(); 
+    });
+});
+
+module.exports = function initGallery(){
+    $(document).ready(function() {
+        $("#lightgallery").lightGallery(); 
+    });
+}
+
 const $photoTemplate = $('#photo-template').html();
 const $gallery = $('#lightgallery');
 
@@ -10,3 +22,5 @@ module.exports = function renderGallery(data) {
 function generatePhoto(el) {
     return $photoTemplate.replace('{{photoUrl}}', el.thumbnailUrl);
 }
+
+ 
